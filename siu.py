f0d164ca1a88697bb3d12a68ed187fe99fa539fd
dqshahab <ridershahab56@gmail.com>
2023-05-16 00:18:03 +0530

    reply updated

--- siu.py
+++ siu.py
@@ -1,5 +1,5 @@
 z=int(input("enter the jersey no of Ronaldo : "))
 if z == 7:
-    print("Cristiano Ronaldo is the GOAT in the world")
+    print("Cristiano is the GOAT in the world \nRonaldo has 5 champions league and Morocco don't have 5 champions league")
 else:
     print("entered number is wrong, please re-enter again!")
