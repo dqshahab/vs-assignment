579708282294067bd6bcbabd8a4d82a6709d7461
dqshahab <ridershahab56@gmail.com>
2023-05-15 23:57:51 +0530

    first commit

---
+++ siu.py
@@ -0,0 +1,5 @@
+z=int(input("enter the jersey no of Ronaldo : "))
+if z == 7:
+    print("Cristiano Ronaldo is the GOAT in the world")
+else:
+    print("entered number is wrong, please re-enter again!")
